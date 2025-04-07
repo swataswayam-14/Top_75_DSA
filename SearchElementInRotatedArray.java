8a8c0afa45812a688373c5214076b27510055def
Swata Swayam Dash <swataswayamdash@gmail.com>
2025-04-08 00:14:08 +0530

    search element in rotated array

---
+++ SearchElementInRotatedArray.java
@@ -0,0 +1,47 @@
+public class SearchElementInRotatedArray {
+    boolean searchBrute(int arr[], int n , int k) {
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == k) return true;
+        }
+        return false;
+    }
+    boolean searchInRotatedSortedArray(int arr[], int k) {
+        int n = arr.length;
+        int low = 0, high = n-1;
+        while (low <= high) {
+            int mid = (low + high)/2;
+            if (arr[mid] == k) return true;
+            if (arr[mid] == arr[mid] && arr[mid] == arr[high]) {
+                low = low+1;
+                high = high-1;
+                continue;
+            }
+            //checking if the left part is sorted
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] <= k && k <= arr[mid]){ // element exists in this part
+                    high = mid-1;
+                } else { //element does not exist
+                    low = mid+1;
+                }
+            } else { //the right part is sorted
+                if (arr[mid] <= k && k <= arr[high]) {
+                    low = mid + 1;
+                } else { //element does not exist
+                    high = mid-1;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] arr = {7, 8, 1, 2, 3, 3, 3, 4, 5, 6};
+        int k = 3;
+        SearchElementInRotatedArray ob = new SearchElementInRotatedArray();
+        boolean ans1 = ob.searchInRotatedSortedArray(arr, k);
+        boolean ans2 = ob.searchBrute(arr, arr.length, k);
+        if (ans1 == false && ans2 == false)
+            System.out.println("Target is not present.");
+        else
+            System.out.println("Target is present in the array.");
+    }
+}
