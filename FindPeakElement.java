930ea8f412cae37c105f2a95bb4ae32b04f1fd1a
Swata Swayam Dash <swataswayamdash@gmail.com>
2025-04-08 18:42:51 +0530

    code for finding peak element in a sorted array

--- FindPeakElement.java
+++ FindPeakElement.java
@@ -20,9 +20,32 @@ public class FindPeakElement {
         }
         return ans;
     }
+    public static int findPeakOptimal(ArrayList<Integer>arr) {
+        int n = arr.size();
+        if (n==0) return 0;
+        if (arr.get(0) > arr.get(1)) return 0;
+        if (arr.get(n-1) > arr.get(n-2)) return n-1;
+
+        int low = 0;
+        int high = n-1;
+        while (low<=high) {
+            int mid = (low+high) / 2;
+            if(arr.get(mid) > arr.get(mid+1) && arr.get(mid) > arr.get(mid-1)) return mid;
+            if (arr.get(mid) > arr.get(mid-1)) {
+                low = mid+1; //eliminate left part of the array
+            } else {
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
         int[] testArray = {1, 3, 20, 4, 1, 0, 5, 3, 6};
         ArrayList<Integer> peaks = findPeakBrute(testArray);
         System.out.println("Peaks found: " + Arrays.toString(peaks.toArray()));
+        ArrayList<Integer> arr =
+            new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 5, 1));
+        int ans = findPeakOptimal(arr);
+        System.out.println("The peak is at index: " + ans);
     }
 }
