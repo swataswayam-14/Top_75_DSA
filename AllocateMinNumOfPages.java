c5e02f6a74244b77b250d1a929ee43d49c5c3f58
Swata Swayam Dash <swataswayamdash@gmail.com>
2025-04-09 14:05:35 +0530

    allocate min number of pages to student optimal code added

--- AllocateMinNumOfPages.java
+++ AllocateMinNumOfPages.java
@@ -15,6 +15,19 @@ public class AllocateMinNumOfPages {
         }
         return low;
     }
+    public static int findPagesOptimal(ArrayList<Integer> arr, int n, int m) {
+        int low = Collections.max(arr);
+        int high = arr.stream().mapToInt(Integer::intValue).sum();
+        while (low <= high) {
+            int mid = (low +high) /2;
+            if (countStudents(arr, mid) > m){ // the value needs to be increased (right half)
+                low = mid+1;
+            } else {
+                high = mid -1;
+            }
+        }
+        return low;
+    }
     public static int countStudents(ArrayList<Integer> arr, int pages) {
         int n = arr.size();
         int students = 1;
@@ -33,7 +46,9 @@ public class AllocateMinNumOfPages {
         ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(25, 46, 28, 49, 24));
         int n = 5;
         int m = 4;
-        int ans = findPages(arr, n, m);
-        System.out.println("The answer is: " + ans);
+        int ans1 = findPages(arr, n, m);
+        System.out.println("The answer is: " + ans1);
+        int ans2 = findPagesOptimal(arr, n, m);
+        System.out.println("The answer is: " + ans2);
     }
 }
